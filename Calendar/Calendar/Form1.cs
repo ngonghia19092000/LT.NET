c4673597feac4292bb95ed089aee4bc3c94803c6
dukhanh <18130108@st.hcmuaf.edu.vn>
2020-12-26 18:20:48 +0700

    kfajfk

--- Calendar/Calendar/Form1.cs
+++ Calendar/Calendar/Form1.cs
@@ -258,7 +258,7 @@ namespace Calendar
 
 
         }
-        // command 
+        // khanh co mommand moi
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             SerializeToXML(Job, filePath);
